e0c3915fe57d1cec82fae4127e6b5c1bb038ef2c
Silas <silas@thecoven.co.uk>
2020-08-06 11:30:49 +0100

    addedscreenshot renderer

--- js/site.js
+++ js/site.js
@@ -5,24 +5,40 @@ let engine = {
 	 */
 	
 	splashscreen : [
-		"https://github.com/sjewitt/doom_snippets/blob/master/screenshots/Screenshot_Doom_20200523_223655.png?raw=true",
-		"https://github.com/sjewitt/doom_snippets/blob/master/screenshots/Screenshot%20from%202020-07-25%2008-25-00.png?raw=true",
-		"https://raw.githubusercontent.com/sjewitt/doom_snippets/master/screenshots/Screenshot%20from%202020-07-24%2020-41-02.png",
-		"https://github.com/sjewitt/doom_snippets/blob/master/screenshots/slope.png?raw=true",
-		"https://github.com/sjewitt/doom_snippets/blob/master/screenshots/Screenshot%20from%202020-07-24%2020-38-07.png?raw=true",
-		"https://github.com/sjewitt/doom_snippets/blob/master/screenshots/Screenshot%20from%202020-07-12%2011-26-40.png?raw=true",
-		"https://github.com/sjewitt/doom_snippets/blob/master/screenshots/Screenshot%20from%202020-07-05%2010-07-28.png?raw=true",
-		"https://github.com/sjewitt/doom_snippets/blob/master/screenshots/Screenshot%20from%202020-06-14%2017-01-05.png?raw=true"
+		"Screenshot_Doom_20200523_223655.png",
+		"Screenshot%20from%202020-07-25%2008-25-00.png",
+		"Screenshot%20from%202020-07-24%2020-41-02.png",
+		"slope.png",
+		"Screenshot%20from%202020-07-24%2020-38-07.png",
+		"Screenshot%20from%202020-07-12%2011-26-40.png",
+		"Screenshot%20from%202020-07-05%2010-07-28.png",
+		"Screenshot%20from%202020-06-14%2017-01-05.png"
 	],
 	
-	imagebase : 'https://github.com/sjewitt/doom_snippets/blob/master/screenshots/',
-	suffix : '?raw=true',
+	imagebase : 'https://github.com/sjewitt/doom_snippets/blob/master/screenshots/',	//the unsorted path
+	imagesortedprefix : 'sorted/',	//prefix to sorted paths
+	imagesuffix : '?raw=true',
 	mapScreenshots : {
-		breachContainment : [
-			
-			
+		breach_containment : [
+			'breachcontainment2020-04-14 17-11-30.png',
+			'breachcontainment2020-04-15 09-35-25.png',
+			'breachcontainment2020-04-15 09-36-13.png',
+			'breach_containment_2020-05-11 10-57-06.png',
+			'breach_containment_2020-05-11 10-57-15.png',
+			'breach_containment_2020-05-19 09-59-32.png',
+			'breach_containment_2020-05-19 10-00-54.png',
+			'breach_containment_2020-05-19 10-01-27.png',
+			'breachcontainment2020-06-13 13-04-01.png',
+			'breachcontainment2020-06-13 13-04-16.png',
+			'breachcontainment2020-06-13 13-04-33.png',
+			'breachcontainment2020-06-13 13-04-54.png',
+			'breachcontainment2020-06-13 13-09-17.png',
+			'breachcontainment_21.png',
+			'breach_containment3.png',
+			'breach_containment4.png',
+			'breach_containmentbreach_containment.png'
 		],
-		breachScars :[
+		breach_scars :[
 			'breach_scars_2020-04-29 10-08-53.png',
 			'breach_scars_2020-04-29 10-08-59.png',
 			'breach_scars_2020-05-11 10-59-14.png',
@@ -30,9 +46,35 @@ let engine = {
 			'breach_scars_20200523_223655.png'
 		],
 		
-		theGate : [
-			
-			
+		the_gate : [
+			'thegate2020-05-11 17-05-57.png',
+			'thegate2020-06-01 10-56-54.png',
+			'thegate2020-05-20 17-32-06.png',
+			'thegate2020-06-01 11-55-51.png',
+			'thegate2020-05-23 12-55-24.png',
+			'thegate2020-06-01 12-11-23.png',
+			'thegate2020-05-23 12-59-44.png',
+			'thegate2020-06-01 12-11-43.png',
+			'thegate_20200523_225015 (1).png',
+			'thegate2020-06-01 17-34-32.png',
+			 'thegate20200523_225107.png',
+			'thegate2020-06-01 17-36-27.png',
+			 'thegate_20200523_225210.png',
+			'thegate2020-06-01 17-38-53.png',
+			 'thegate_20200523_225249.png',
+			'thegate2020-06-03 15-11-55.png',
+			'thegate20200523_225307.png',
+			'thegate2020-06-03 15-16-16.png',
+			'thegate2020-05-25 09-55-35.png',
+			'thegate2020-06-13 13-50-28.png',
+			'thegate2020-05-25 10-17-14.png',
+			'thegate2020-06-13 13-52-56.png',
+			'thegate2020-05-31 09-05-02.png',
+			'thegate2020-06-13 13-53-11.png',
+			'thegate2020-05-31 09-07-00.png',
+			'thegate2020-06-13 13-53-27.png',
+			'thegate2020-06-01 10-56-44.png',
+			'thegate2020-06-13 13-53-59.png'
 		]
 	},
 	
@@ -60,20 +102,7 @@ let engine = {
 		return Math.floor(Math.random() * Math.floor(this.splashscreen.length));
 	},
 	
-	/**
-	Build simple DOM for image
-	 */
-	getImageDOM : function(imgUrl){
-		let _img = document.createElement("img");
-		_img.setAttribute('src',imgUrl);
-		_img.setAttribute('alt','Random Doom map image');
-		let _a = document.createElement("a");
-		_a.setAttribute('href',imgUrl);
-		_a.setAttribute('title','Click to view fullsize image');
-		_a.setAttribute('target','_blank');
-		_a.appendChild(_img);
-		return(_a);
-	},
+	
 	
 	/**
 	append click handlers to all screenshot togglers:
@@ -93,11 +122,54 @@ let engine = {
 	
 	buildScreenshotImages : function(){
 		for( prop in this.mapScreenshots){
+			console.log(prop);
 			console.log(this.mapScreenshots[prop]);
-			/* iterate over each sub-array, and append each to the corresponding DOM ID element, Remove first... */
-			//}
+			/* 
+			iterate over each sub-array, and append each to the corresponding DOM ID element, Remove first... 
+			The data structure top level properties are the map name, and map to the path of the graphics for 
+			specified map. 
+			*/
+			$('#'+prop).empty();
+			for(let a=0;a<this.mapScreenshots[prop].length;a++){
+				/* Now pass each image to the builder method: */
+				console.log(this.mapScreenshots[prop][a],prop);
+				$('#'+prop).append(this.getImageDOM(this.mapScreenshots[prop][a],prop));
+			}
+
 		}
-	}
+	},
+	
+	/**
+	Build simple DOM for image
+	 */
+	getImageDOM : function(imgName, pathSuffix){
+		console.log('getting image ' + imgName, pathSuffix);
+		/** build the full URL - start with the defined base path */
+		let _url = this.imagebase;
+		console.log('base URL: ' + _url);
+		if(pathSuffix){
+			/** if we are passing a path part, add it on: */
+			//_url = _url + pathSuffix + "/";
+			
+			_url = _url +this.imagesortedprefix  + pathSuffix+ "/" + imgName + this.imagesuffix;
+			console.log('pathed URL: ' + _url);
+		}
+		else{
+			_url = _url + imgName + this.imagesuffix;
+		}
+		
+		
+		console.log('full URL: ' + _url);
+		let _img = document.createElement("img");
+		_img.setAttribute('src',_url);
+		_img.setAttribute('alt','Random Doom map image');
+		let _a = document.createElement("a");
+		_a.setAttribute('href',_url);
+		_a.setAttribute('title','Click to view fullsize image');
+		_a.setAttribute('target','_blank');
+		_a.appendChild(_img);
+		return(_a);
+	},
 	
 }
 
@@ -107,5 +179,19 @@ load pages
 $(function(){
 	engine.init();
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
