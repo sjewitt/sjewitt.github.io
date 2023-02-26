a06961dbcdf4b0803d799ff046d8c1390f109411
sjewitt <6630247+sjewitt@users.noreply.github.com>
2023-02-26 15:05:29 +0000

    Add files via upload

--- js/engine.js
+++ js/engine.js
@@ -20,68 +20,83 @@ let engine = {
  data is not that order...
  
  */
+/** TODO: make self-contained data structure that is not dependent on ORDER */
+	all_data : {
+		'q1':{'title':'Product &amp; service design', 'description':'Includes Proposition Design, Product Design, Digital and Physical Experience Design'},
+		'q2':{'title':'Management &amp; leadership', 'description':'How we influence people to get stuff done'},
+		'q3':{'title':'Capability building', 'description':'Providing clients with the tools they need to achieve their goals.'},
+		'q4':{'title':'Transforming organizations', 'description':'What skills do you have that can support our clients in their ambitions to change their business?'}
+	},
+	
+	rating_description_lookup : [
+		'unrated',
+		'noob',
+		'getting to grips',
+		'competent',
+		'pretty good',
+		'excellent',
+		'ninja'
+	],
+	
+	/** quadrant description and title */
 	data_0 : [
-	    'Includes Proposition Design, Product Design, Digital and Physical Experience Design',
-	    'How we influence people to get stuff done',
-	    'Providing clients with the tools they need to achieve their goals.',
-	    'What skills do you have that can support our clients in their ambitions to change their business?'
+	    {'description':'Includes Proposition Design, Product Design, Digital and Physical Experience Design','title':'Product & service design'},
+	    {'description':'How we influence people to get stuff done','title':'Management & leadership'},
+	    {'description':'Providing clients with the tools they need to achieve their goals.','title':'Capability building'},
+	    {'description':'What skills do you have that can support our clients in their ambitions to change their business?','title':'Transforming organizations'}
 	],
 
-	/**
-	 * sector title descriptions within each quadrant
-	 */
+	/** sector title descriptions within each quadrant */
 	data_1 : [
 	    [
-	        'Understanding how to originate, encourage and communicate complex ideas and concepts with simplicity and clarity.',
-	        'Ability to use tools that will reduce uncertainty and test hypotheses',
-	        'Skills in the creation and definition of functional, usable and simplified user experiences',
-	        'Skills in the creation of beautiful and functional things using the right tools.',
-	        'Skills in the creation of interactions over time that make our clients stand out in the minds of their customers'
+	        {'description': 'Understanding how to originate, encourage and communicate complex ideas and concepts with simplicity and clarity.', 'title':'Ideas & concepts'},
+	        {'description': 'Ability to use tools that will reduce uncertainty and test hypotheses', 'title':'Prototypes & experiments'},
+	        {'description': 'Skills in the creation and definition of functional, usable and simplified user experiences', 'title':'UX design'},
+	        {'description': 'Skills in the creation of beautiful and functional things using the right tools.', 'title':'Visual design'},
+	        {'description': 'Skills in the creation of interactions over time that make our clients stand out in the minds of their customers', 'title':'Service design'}
 	    ],
 	    [
-	        'Building effective, successful and long lasting relationships',
-	        'Understanding the impact of decisions we make on the business from a financial, risk and regulatory perspective.',
-	        'Effective use of the tools and rituals involved in running lean and agile projects with a focus on people and results',
-	        'Skills in running virtual and in person workshops to smoothly and simply achieve the stated goals.'
+	        {'description': 'Building effective, successful and long lasting relationships','title':'Client & account management'},
+	        {'description': 'Understanding the impact of decisions we make on the business from a financial, risk and regulatory perspective.','title':'Commerials'},
+	        {'description': 'Effective use of the tools and rituals involved in running lean and agile projects with a focus on people and results','title':'Agile PM/coaching'},
+	        {'description': 'Skills in running virtual and in person workshops to smoothly and simply achieve the stated goals.','title':'Facilitation'}
 	    ],
 	    [
-			'Know how to enable our clients businesses to become more innovative',
-			'Understand the skills our client needs to exceed expectations in their next role. Know how to support them to get there.',
-	        'Ability to share knowledge and guide people to reach their full potential',
-	        'Creation and delivery of training in our tools and techniques',
+			{'description': 'Know how to enable our clients businesses to become more innovative','title':'Innovation capabilities'},
+			{'description': 'Understand the skills our client needs to exceed expectations in their next role. Know how to support them to get there.','title':'Leadership readiness'},
+	        {'description':  'Ability to share knowledge and guide people to reach their full potential','title':'Mentoring & coaching'},
+	        {'description':  'Creation and delivery of training in our tools and techniques','title':'Innovation training'}
 	        
 	    ],
 	    [
-	        'How you research, define and bring to life the desired future state of our client.',
-	        'Using powerful storytelling to influence the behaviours of people who make up the client’s business.',
-	        'Understanding which tools will accelerate the client’s ability to achieve their future vision and using them to predict the effect on customer behaviour.',
-	        'Use of varied research skills to understand the needs, wants and desires of customers.'
+	        {'description': 'How you research, define and bring to life the desired future state of our client.','title':'Strategy & vision'},
+	        {'description': 'Using powerful storytelling to influence the behaviours of people who make up the client’s business.','title':'Culture & experience'},
+	        {'description':  'Understanding which tools will accelerate the client’s ability to achieve their future vision and using them to predict the effect on customer behaviour.','title':'Business design & growth'},
+	        {'description':  'Use of varied research skills to understand the needs, wants and desires of customers.','title':'Customer insights'}
 	    ],
 	],
 
-	/** 
-	 * sector area descriptions - applies to all concentric levels
-	 */
+	/** sector area descriptions - applies to all concentric levels */
 	data_2 : [
 	    [
 	        'Has experience of using a variety of tools to generate ideas and concepts. Can plan and lead an ideation workshop. Understands how to create \'just enough\' to show the viability / art of the possible in the idea we are communicating.',
 	        'Use tools that will reduce uncertainty and test hypotheses. Understand the different types of experiments and their suitability. Communicate the plan, hypothesis and outcomes with clients Analyse results in various formats.',
 	        'Independently create and define functional, usable and simplified user experiences. Understand of the importance of designing for accessibility Communicate the thought behind the design and work collaboratively to achieve sign off.',
 	        'Shape and drive of visual outcomes throughout the course of a project Communicate the thought behind the design and work collaboratively to achieve sign off. Review the work of junior designers and give feedback to improve',
-	        'Own end-to-end user journey Understand the user needs and foresee painpoints to deliver desiered organisational needs Ability to communicate the thought behind the design and work collaboratively to achieve sign off.'
+	        'Own end-to-end user journey Understand the user needs and foresee painpoints to deliver desired organisational needs Ability to communicate the thought behind the design and work collaboratively to achieve sign off.'
 	    ],
 	    [
-	        'Demonstrate the ability to plan lead, facilitate, and deliver a workshop with positive, on-brief outcomes Lead and sign off on the work of others.',
-	        'Coach others and gain buy-in from clients and teams (client and internal) for Agile ways of working. Deliver medium-sized projects successfully. Manage simultaneous engagements successfully',
 	        'build relationships with prospective clients, maintain relationships with existing clients, regularly share interesting insights with their network, spot and qualify opportunities. broker introductions to relevant Magnets and others in the Magnetic network.',
-	        'Liasie effectively and in a timely manner with finance, produce high-quality commercial documentation and reports such as SOW and project tracker. Keep track of the numbers week in and week out. Understanding of contracts and procurement. They understand the impact of commercial decisions on the wider business,'
+			'Liasie effectively and in a timely manner with finance, produce high-quality commercial documentation and reports such as SOW and project tracker. Keep track of the numbers week in and week out. Understanding of contracts and procurement. They understand the impact of commercial decisions on the wider business',
+	        'Coach others and gain buy-in from clients and teams (client and internal) for Agile ways of working. Deliver medium-sized projects successfully. Manage simultaneous engagements successfully',
+	        'Demonstrate the ability to plan lead, facilitate, and deliver a workshop with positive, on-brief outcomes Lead and sign off on the work of others.'
 	    ],
 	    [
+			'Is able to assess the current innovation capability of a department / group / team and advise on what structural changes and training is needed to enable them to become more innovative.',
+			'Role models soft skills, values and behaviours the clients can emulate (Setting a great example). Receive feedback from clients saying they\'ve learned by doing when they worked with you. Confidently coach others',
+			'Share their skills with colleagues and set a good example as needed. Actively listen to colleagues and asks questions to help guide them to their own solutions. Understands the difference between coaching and mentoring and when to use that approach',
 	        'Is able to lead training sessions on our core tools. Is confident in using real life stories in training. Gets the balance right between showing and telling and leads training sessions with a focus on interaction and discussion over lecturing and slideware. Is able to pivot a session to meet the requirements of the trainees.',
-	        'Share their skills with colleagues and set a good example as needed. Actively listen to colleagues and asks questions to help guide them to their own solutions. Understands the difference between coaching and mentoring and when to use that approach',
-	        'Role models soft skills, values and behaviours the clients can emulate (Setting a great example). Receive feedback from clients saying they\'ve learned by doing when they worked with you. Confidently coach others',
-	        'Is able to assess the current innovation capability of a department / group / team and advise on what structural changes and training is needed to enable them to become more innovative.'
-	    ],
+   	    ],
 	    [
 	        'Define the strategic change for a project within a well defined scope. Define the strategy for a part of a business or product',
 	        'Independently create a plan for influencing cultural change. Use tools like experience mapping to define and plan a new or improved experience. Be able to use storytelling to influence adoption of new behaviours.',
@@ -89,128 +104,165 @@ let engine = {
 	        'Independently carry out customer interviews and actively participate in synthesis sessions. Contribute to creation of discussion guides and development of other research/insight techniques. Carry out research on customers within a well understood scope. Research and create insights about an aspect of a service'
 	    ]
 	],
-	
-	/**
-	 * ratings  TODO:
-	 */
-	data_3 : [
-	    [[],[],[],[],[],[]],
-	],
 
-
-	/* element id data */
+	/* map element id data */
 	elems : [
 		'q-1',
 			's1-t',
-				's1-1',
-				's1-2',
-				's1-3',
-				's1-4',
-				's1-5',
-				's1-6',
+				's1-1', 's1-2', 's1-3', 's1-4', 's1-5', 's1-6',
 			's2-t',
-				's2-1',
-				's2-2',
-				's2-3',
-				's2-4',
-				's2-5',
-				's2-6',
+				's2-1', 's2-2', 's2-3', 's2-4', 's2-5', 's2-6',
 			's3-t',
+				's3-1', 's3-2', 's3-3', 's3-4', 's3-5', 's3-6',
 			's4-t',
+				's4-1', 's4-2', 's4-3', 's4-4', 's4-5', 's4-6',
 			's5-t',
+				's5-1', 's5-2', 's5-3', 's5-4', 's5-5', 's5-6',
 		'q-2',
 			's6-t',
+				's6-1', 's6-2', 's6-3', 's6-4', 's6-5', 's6-6',
 			's7-t',
+				's7-1', 's7-2', 's7-3', 's7-4', 's7-5', 's7-6',
 			's8-t',
+				's8-1', 's8-2', 's8-3', 's8-4', 's8-5', 's8-6',
 			's9-t',
-		
-	
+				's9-1', 's9-2', 's9-3', 's9-4', 's9-5', 's9-6',
+
 		'q-3',
 			's10-t',
+				's10-1', 's10-2', 's10-3', 's10-4', 's10-5', 's10-6',
 			's11-t',
+				's11-1', 's11-2', 's11-3', 's11-4', 's11-5', 's11-6',
 			's12-t',
+				's12-1', 's12-2', 's12-3', 's12-4', 's12-5', 's12-6',
 			's13-t',
-	
+				's13-1', 's13-2', 's13-3', 's13-4', 's13-5', 's13-6',
+				
 		'q-4',
 			's14-t',
+				's14-1', 's14-2', 's14-3', 's14-4', 's14-5', 's14-6',
 			's15-t',
+				's15-1', 's15-2', 's15-3', 's15-4', 's15-5', 's15-6',
 			's16-t',
-			's17-t'
+				's16-1', 's16-2', 's16-3', 's16-4', 's16-5', 's16-6',
+			's17-t',
+				's17-1', 's17-2', 's17-3', 's17-4', 's17-5', 's17-6'
 		],
-		
+	
+	current_quad : -1,
+	current_sector : -1,
+	current_score : -1,
+	current_rating : -1,
+	
+	current_data : {},
+	
 	init : function(){
-		console.log('starting...');
 		for(id of this.elems){
 			elem = document.getElementById(id)
 			elem.addEventListener('mouseover',this.test_in)
 			elem.addEventListener('mouseout',this.test_out);
 			elem.addEventListener('click',this.test_handler)
-		}	
+		};
+		this.current_data = [];	
 	},
 	
 	test_handler : function(){
-		console.log('shove it in a database...');
+		if(this.current_score > -1){
+			engine.addToUserdata([this.current_quad,this.current_sector,this.current_score], this.current_rating)
+		}
 	},
-	
+
 	test_in : function(){
-		console.log('IN')
 		let sector_rating = -1;
 	    let lookup = JSON.parse(this.getAttribute('data-lookup')); // expects a string of '[n,n,n]' where n is integer
+	    
+	    this.current_quad = lookup[0];
+		this.current_sector = lookup[1];
+		this.current_score = lookup[2];
+	    
 	    /** quadrant */
 	    let quad_description = false;
-	    console.log(lookup)
+	    let quad_title = false;
+	    let sector_title = '';
 	    if(lookup[0] > -1){
-			console.log('data_0: quadrant ',lookup[0])
-	        quad_description = engine.data_0[lookup[0]];
+		    quad_description = engine.data_0[lookup[0]].description;
+	        quad_title = engine.data_0[lookup[0]].title;
 	    }
 	    
 	    /** sector titles */
-	    let sector_title_description = false;
+	    let sector_title_description = '';
 		if(lookup[1] > -1){
-			console.log('data_1: sector ',lookup[0],lookup[1])
-		    sector_title_description = engine.data_1[lookup[0]][lookup[1]];
+		    sector_title_description = engine.data_1[lookup[0]][lookup[1]].description;
+		    sector_title = engine.data_1[lookup[0]][lookup[1]].title;
 		}
 		
 		/** sector blocks (all) */
-		let sector_block_description = false;
+		let sector_block_description = '';
 		if(lookup[2] > -1){
+			sector_title = engine.data_1[lookup[0]][lookup[1]].title;
 			sector_rating = parseInt(this.getAttribute('data-rating'));
-			console.log('data_2: sector ',lookup[2], 'rating: ',sector_rating)
+			this.current_rating = sector_rating;
 		    sector_block_description = engine.data_2[lookup[0]][lookup[2]];
 		}
-		let outputData = [quad_description,sector_title_description,sector_block_description]
-		console.log(quad_description);
-		console.log(sector_title_description);
-		console.log(sector_block_description);
-		
-		let output = '';
+
 		let output_rating = '';
 		if(sector_rating > -1){
-			output_rating = sector_rating;
+			output_rating = engine.rating_description_lookup[sector_rating];
 		}
-		for(thing of outputData){
-			if(thing){
-				output += thing + '\n\n'
-			}
-		}
-		
-		//document.getElementById('description').innerText = this.getAttribute('data-description');
-		//document.getElementById('rating').innerText = this.getAttribute('data-rating');
-		document.getElementById('description').innerText = output;
+
+		document.getElementById('quad_title').innerText = quad_title;
+		document.getElementById('quad_description').innerText = quad_description;
+		document.getElementById('sector_title').innerText = sector_title;
+		document.getElementById('sector_title_description').innerText = sector_title_description;
+		document.getElementById('sector_block_description').innerText = sector_block_description;
 		document.getElementById('rating').innerText = output_rating;
-//		quad_description 
-//		+  "\n " + sector_title_description
-//		+  "\n " + sector_block_description;
-		console.log(engine.data_1[lookup[0]][lookup[1]])
 	},
 	
 	test_out : function(){
-		console.log('OUT')
-		document.getElementById('description').innerText = "";
 		document.getElementById('rating').innerText = "";
+	},
 	
+	addToUserdata : function(lookup,rating){
+		console.log(lookup,rating);
+		//if(lookup[2]>-1){
+			let append = true;
+			for(let a=0;a<engine.current_data.length;a++){
+				if(engine.current_data[a].key[0] === lookup[0] && engine.current_data[a].key[1] === lookup[1] && engine.current_data[a].key[2] === lookup[2]){
+					append = false;
+					console.log('updating');
+					engine.current_data[a].rating = rating;
+				}
+			}
+			if(append){
+				console.log('appending');
+				engine.current_data.push({'key':lookup,'rating':rating})
+			}
+			this.renderRatings();
+		//}
+	},
+	
+	renderRatings : function(){
+		console.log(engine.current_data);
+		let target = document.getElementById('userdata');
+		target.innerText = "";
+		engine.current_data.sort((a,b) => (a.key[0] > b.key[0]) ? 1 : -1);
+		for(let a=0;a<engine.current_data.length;a++){
+			console.log('sector:',engine.current_data[a].key[0])
+			console.log('segment title data:',engine.current_data[a].key[1])
+			console.log('segment block data:',engine.current_data[a].key[2])
+			console.log('rating:',engine.current_data[a].rating)
+			let row = document.createElement('div');
+			row.appendChild(document.createTextNode(
+				engine.data_0[engine.current_data[a].key[0]].title
+				+', '
+				+engine.data_1[engine.current_data[a].key[0]][engine.current_data[a].key[1]].title
+				+': '
+				+engine.rating_description_lookup[engine.current_data[a].rating]
+			))
+			console.log(engine.data_0[engine.current_data[a].key[0]])
+			target.appendChild(row);
+		}
 	},
 }
-
 
 engine.init();
